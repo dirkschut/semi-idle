ede3d04dd7354241d1960873f393a42ffb18da9b
Dirk Schut <dirk@mrdls.com>
2016-08-17 19:03:55 +0200

    Increasing coins counter

---
+++ Assets/scripts/InterfaceManager.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class InterfaceManager : MonoBehaviour {
+
+	private Player player;
+	private Text coinsAmount;
+
+	// Use this for initialization
+	void Start () {
+		player = new Player ();
+		coinsAmount = GameObject.Find("Coins amount").GetComponent<Text> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		player.coins++;
+		coinsAmount.text = "Coins: " + player.coins;
+	}
+}
---
+++ Assets/scripts/Player.cs
@@ -0,0 +1,8 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player {
+
+	public int coins = 10;
+
+}
